72c42f0d79c5aa9e0c6bbfb8af41d80f050f0876
Maazin Sherif <maazinsherif99@gmail.com>
2024-09-30 03:58:43 -0400

    Portfolio site 90% done

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'hk-grotesk': ['Hk Grotesk', 'sans-serif'],
+        'jost': ['Jost', 'sans-serif'],
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '7.5rem',
+      },
+      colors: {
+        'pink-600': '#ff3258',
+        'white-1': '#e5e5e6da',
+      },
+ 
+    },
+  },
+  plugins: [],
+}
